0a6b85fd851838f5c46907f20daa4cbce1c93dd2
v3DJG6GL <72495210+v3DJG6GL@users.noreply.github.com>
2025-03-01 00:05:11 +0100

    url-prefix-test-1

--- background.js
+++ background.js
@@ -1,55 +1,66 @@
 function createdMenuItem(n) {
-	if (browser.runtime.lastError) {
-		console.log(`Error: ${browser.runtime.lastError}`);
-	}
+  if (browser.runtime.lastError) {
+    console.log(`Error: ${browser.runtime.lastError}`);
+  }
 }
+
 const menuItem = {
-	id: "copy_url",
-	title: browser.i18n.getMessage("contextItemTitle"),
-	contexts: ["tab"],
+  id: "copy_url",
+  title: browser.i18n.getMessage("contextItemTitle"),
+  contexts: ["tab"],
 };
 browser.contextMenus.create(menuItem, createdMenuItem);
 
-function copyUrl(sender, tabs) {
-	const urls = tabs.map((tab) => {
-		return tab.url;
-	});
-	if (urls.length > 0) {
-		urls_str = urls.join("\n");
-		navigator.clipboard.writeText(urls_str);
-	}
+// Function to get the URL prefix from storage
+async function getURLPrefix() {
+  const result = await browser.storage.sync.get("urlPrefix");
+  return result.urlPrefix || ""; // Return empty string if no prefix is set
 }
 
-const tabsQuery = { currentWindow: true, highlighted: true };
+async function copyUrl(sender, tabs) {
+  const urls = tabs.map((tab) => {
+    return tab.url;
+  });
 
-browser.contextMenus.onClicked.addListener(function (info, sender) {
-	const querying = browser.tabs.query(tabsQuery);
-	querying.then(copyUrl.bind(null, sender));
-});
+  if (urls.length > 0) {
+    const urlPrefix = await getURLPrefix(); // Get the URL prefix
+    const urlsWithPrefix = urls.map(url => urlPrefix + url); // Apply the prefix to each URL
+    urls_str = urlsWithPrefix.join("\n");
+    navigator.clipboard.writeText(urls_str);
+
+    const tabsQuery = { currentWindow: true, highlighted: true };
+    browser.contextMenus.onClicked.addListener(function (info, sender) {
+      const querying = browser.tabs.query(tabsQuery);
+      querying.then(copyUrl.bind(null, sender));
+    });
+  }
+}
 
 // supports Tree Style Tab's fake context menu
 // https://addons.mozilla.org/firefox/addon/tree-style-tab/
 function registerToTST() {
-	browser.runtime
-		.sendMessage("treestyletab@piro.sakura.ne.jp", {
-			type: "fake-contextMenu-create",
-			params: menuItem,
-		})
-		.then(createdMenuItem, createdMenuItem);
+  browser.runtime
+    .sendMessage("treestyletab@piro.sakura.ne.jp", {
+      type: "fake-contextMenu-create",
+      params: menuItem,
+    })
+    .then(createdMenuItem, createdMenuItem);
+
+  browser.runtime.onMessageExternal.addListener((message, sender) => {
+    switch (sender.id) {
+      case "treestyletab@piro.sakura.ne.jp":
+        switch (message.type) {
+          case "ready":
+            registerToTST();
+            return;
+          case "fake-contextMenu-click":
+            browser.tabs
+              .query(tabsQuery)
+              .then((tabs) => copyUrl(message.tab, tabs));
+            return;
+        }
+    }
+  });
 }
-browser.runtime.onMessageExternal.addListener((message, sender) => {
-	switch (sender.id) {
-		case "treestyletab@piro.sakura.ne.jp":
-			switch (message.type) {
-				case "ready":
-					registerToTST();
-					return;
-				case "fake-contextMenu-click":
-					browser.tabs
-						.query(tabsQuery)
-						.then((tabs) => copyUrl(message.tab, tabs));
-					return;
-			}
-	}
-});
+
 registerToTST();
